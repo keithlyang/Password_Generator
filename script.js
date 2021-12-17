d88cf8468d8b91b2fe7d269db7bc354350ec563d
keithlyang <keithluckiyang@gmail.com>
2021-12-16 18:56:15 -0600

    intitial commit

--- script.js
+++ script.js
@@ -10,5 +10,30 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+  prompt("How many characters would you like your password to contain?")
+    if (password >=8) {
+      console.log("OK");
+    } else ("Password length must be at least 8 characters long.")
+    }
+
+    if (password <128) {
+      console.log();
+    }
+
+  // return ""
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// var input = [2, 34, 5, 67, 44, 101];
+
+// var output = []
+
+// if (input [0] % 2 === 0){
+//    output.push("even") 
+// } else {
+//   output.push("odd")
+// }

f1545a4a4e6e1ccb0a291bca7163066c2bcf4848
keithlyang <keithluckiyang@gmail.com>
2021-12-16 23:49:16 -0600

    initial commit

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var passwordLength = prompt("Password must be between 8 to 128 characters.")
+  var length;
+  var lowerCase;
+  var upperCase;
+  var numbers;
+  var specialCharacters;
+  var numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",  "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var specialCharactersArray = ["!", " ” ", "#", "$", "%", "&", " ’ ", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\ ", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var useableCharactersArray = [];
 
   passwordText.value = password;
 
